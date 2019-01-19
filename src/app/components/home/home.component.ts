f0166da5aa8ccb231a32ad2ac01dc124e616c7d0
Jan Kuri <jkuri88@gmail.com>
2019-01-19 05:56:22 +0100

    chore(): fix table for mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,8 +5,7 @@ import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   fetching: boolean;
--- src/app/components/player-item/player-item.component.ts
+++ src/app/components/player-item/player-item.component.ts
@@ -3,8 +3,7 @@ import { Player } from '../../shared/player.class';
 
 @Component({
   selector: 'app-player-item',
-  templateUrl: './player-item.component.html',
-  styleUrls: ['./player-item.component.sass']
+  templateUrl: './player-item.component.html'
 })
 export class PlayerItemComponent implements OnInit {
   @Input() player: Player;
