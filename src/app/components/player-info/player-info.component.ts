fbae09c34d88a2065f995435ab96fc4b9cbeee9a
Jan Kuri <jkuri88@gmail.com>
2019-01-20 23:42:47 +0100

    chore(): construct player info data

--- src/app/components/player-info/player-info.component.ts
+++ src/app/components/player-info/player-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayersService } from '../../shared/players.service';
 import { ActivatedRoute } from '@angular/router';
+import { Player } from '../../shared/player.class';
 
 @Component({
   selector: 'app-player-info',
@@ -9,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class PlayerInfoComponent implements OnInit {
   fetching: boolean;
   id: number;
+  player: Player;
 
   constructor(
     public playersService: PlayersService,
@@ -24,6 +26,22 @@ export class PlayerInfoComponent implements OnInit {
     this.fetching = true;
     this.playersService.fetchPlayer(this.id)
       .subscribe(data => {
+        const player = data.info;
+        this.player = new Player(
+          player.id,
+          player.name,
+          player.lastname,
+          new Date(player.birthdate),
+          Number(player.height),
+          Number(player.weight),
+          player.club,
+          player.avatar,
+          Number(player.number),
+          player.position,
+          player.birthplace,
+          player.notes
+        );
+
         console.log(data);
       }, err => console.error(err), () => this.fetching = false);
   }
